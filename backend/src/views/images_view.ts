cebe645f883a13ec0466e69a0d86b4e770d2b2fe
Igor <igorbitencourtsilveira@gmail.com>
2020-10-22 18:38:58 -0300

    Modificação para que o endereço da API seja definido em um arquivo .env

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -1,10 +1,11 @@
 import Image from "../models/Image"
+require('dotenv').config()
 
 export default{
     render(image: Image){
         return{
             id: image.id, 
-            url: `http://localhost:3333/uploads/${image.path}`
+            url: `${process.env.SERVER_HOST}/uploads/${image.path}`
         }
     },
 
