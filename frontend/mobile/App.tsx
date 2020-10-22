cdadd6b659079983cb5c9427cae76cb63afed8d6
Igor <igorbitencourtsilveira@gmail.com>
2020-10-22 18:43:04 -0300

    Modificação na versão mobile para que o endereço da API seja definido em um arquivo .env

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useFonts } from "expo-font";
 import { Nunito_600SemiBold,Nunito_700Bold,Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
 
+
 // Rotas da aplicação
 import Routes from "./src/routes";
 
@@ -15,8 +16,8 @@ export default function App() {
 
   if(!fontsLoaded){
     return null;
-  }
-
+  } 
+  
   return (
     <Routes/>
   )
